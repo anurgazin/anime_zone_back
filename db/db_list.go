b6476dc38ba5b9e7e6304c32747ff3a99a71291e
anurgazin <dindog24@gmail.com>
2024-10-05 16:35:29 -0400

    Route for CharacterList Added(Post CharacterList)

--- db/db_list.go
+++ db/db_list.go
@@ -26,3 +26,23 @@ func CreateAnimeList(listName string, id string) (interface{}, error) {
 	fmt.Println("Inserted multiple documents: ", insertResult.InsertedID)
 	return insertResult.InsertedID, nil
 }
+
+func CreateCharacterList(listName string, id string) (interface{}, error) {
+	var characterList CharacterList
+	client := RunMongo()
+	collection := client.Database("Anime-Zone").Collection("CharacterList")
+	characterList.ID = primitive.NewObjectID()
+	userId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid UserId format: %w", err)
+	}
+	characterList.UserID = userId
+	characterList.Name = listName
+	insertResult, err := collection.InsertOne(context.TODO(), characterList)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	fmt.Println("Inserted multiple documents: ", insertResult.InsertedID)
+	return insertResult.InsertedID, nil
+}
