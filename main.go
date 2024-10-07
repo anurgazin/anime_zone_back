989ea03e9b1847d0579cb57cc766e3582ec4988a
anurgazin <dindog24@gmail.com>
2024-10-07 15:57:32 -0400

    New Route for List added(GET /list/characters/:id)

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	router.GET("/list/anime", routes.GetAnimeLists)
 	router.GET("/list/characters", routes.GetCharacterLists)
 	router.GET("/list/anime/:id", routes.GetAnimeListById)
+	router.GET("/list/characters/:id", routes.GetCharacterListById)
 	router.POST("/list/anime", middleware.AuthToken, routes.PostAnimeList)
 	router.POST("/list/characters", middleware.AuthToken, routes.PostCharacterList)
 	router.PATCH("/list/anime/:id", middleware.AuthToken, routes.AddAnimeToList)
