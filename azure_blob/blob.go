4445f02463e572b1a295069c1e53b6573f3ddad6
anurgazin <dindog24@gmail.com>
2024-10-10 19:37:43 -0400

    Logo and Media uploading for Character added(Post /characters)

--- azure_blob/blob.go
+++ azure_blob/blob.go
@@ -6,12 +6,13 @@ import (
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
 )
 
 const containerName = "animezonecontatiner"
 
 // UploadFile uploads a file to Azure Blob Storage and returns the file URL
-func UploadFile(fileName string, content []byte) (string, error) {
+func UploadFile(fileName string, content []byte, contentType string) (string, error) {
 	// Get the Blob service client
 	serviceClient, err := GetBlobServiceClient()
 	if err != nil {
@@ -28,16 +29,22 @@ func UploadFile(fileName string, content []byte) (string, error) {
 	}
 
 	// Create the blob client and upload the file
-	blobClient := containerClient.NewBlockBlobClient(fileName)
+	blockBlobClient := containerClient.NewBlockBlobClient(fileName)
 
+	// Setting blob properties
+	var httpHeaders = blob.HTTPHeaders{
+		BlobContentType: &contentType,
+	}
 	// Upload the file content
-	_, err = blobClient.UploadBuffer(context.TODO(), content, &azblob.UploadBufferOptions{})
+	_, err = blockBlobClient.UploadBuffer(context.TODO(), content, &azblob.UploadBufferOptions{
+		HTTPHeaders: &httpHeaders,
+	})
 	if err != nil {
 		return "", fmt.Errorf("failed to upload file: %w", err)
 	}
 
 	// Construct the URL for the uploaded file
-	fileURL := blobClient.URL()
+	fileURL := blockBlobClient.URL()
 
 	fmt.Println("File uploaded successfully! URL: ", fileURL)
 	return fileURL, nil
